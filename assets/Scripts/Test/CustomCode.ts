66cfcaa90e8eb4e0223e6962ff8c880b026555a6
LiuyangZhang <543529537@qq.com>
2023-07-07 00:16:20 +0800

    提交测试

---
+++ assets/Scripts/Test/CustomCode.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+
+/**
+ * 
+ * CustomCode
+ * LiuyangZhang
+ * Fri Jul 07 2023 00:14:33 GMT+0800 (中国标准时间)
+ *
+ */
+
+export class CustomCode
+{
+    
+}
